5b79b84c087f13346405fdc749f4c4ce53ff52ee
goodingc <callum.gooding11@gmail.com>
2020-09-29 14:21:07 +0100

    29/09 b

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,11 +57,10 @@ export default {
         /*
          ** You can extend webpack config here
          */
-        extend(config, ctx) {}
     },
 
     server: {
-        port: 3001,
+        port: 3000,
         host: '0.0.0.0'
     },
 
